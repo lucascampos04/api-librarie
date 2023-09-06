6547f1a948259016015ac51745735b0e3dc682e5
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-06 15:49:52 -0300

    crud completo

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -22,6 +22,43 @@ router.post('/add', (req, res) => {
     )
 })
 
+router.put('/update/:id', (req, res) => {
+    const { title, author, isbn } = req.body
+    const bookID = req.params.id
+
+    connection.query(
+        'UPDATE books SET title=?, author=?, isbn=? WHERE id=?',
+        [title, author, isbn, bookID],
+        (error, results) => {
+            if (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Erro ao atualizar livro' });
+            } else {
+                res.status(200).json({ message: 'Livro atualizado com sucesso' });
+                console.log("livro atualizado com sucesso")
+            }
+        }
+    )
+})
+
+router.delete('/delete/:id', (req, res) => {
+    const bookID = req.params.id
+
+    connection.query(
+        'DELETE FROM books WHERE id=?',
+        [bookID],
+        (error, results) => {
+            if (error) {
+                console.error(error)
+                res.send(500).json({ error: 'Error ao remover livro' })
+            } else {
+                res.status(200).json({ message: 'Livro removido com sucesso' })
+                console.log("livro removido com sucesso")
+            }
+        }
+    )
+})
+
 router.get('/', (req, res) => {
     connection.query('SELECT * FROM books', (error, results) => {
         if (error) {
